a54858424dcf0ae9a9de713d77014203830e181a
Thomas Benguigui <tbenguigui@octo.com>
2020-04-03 11:42:16 +0200

    Added logback dependencies and logback config
    Signed-off-by: Thomas Benguigui <tbenguigui@octo.com>

--- src/main/java/com/octo/kata/archiclean/ArchiCleanApplication.java
+++ src/main/java/com/octo/kata/archiclean/ArchiCleanApplication.java
@@ -1,5 +1,7 @@
 package com.octo.kata.archiclean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,8 +10,17 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.octo.kata.archiclean")
 public class ArchiCleanApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger("kata-logback");
+
     public static void main(String[] args) {
         SpringApplication.run(ArchiCleanApplication.class, args);
+
+        logger.warn("LOGBACK KATA - Test WARN message");
+        logger.info("LOGBACK KATA - Test INFO message");
+        logger.error("LOGBACK KATA - Test ERROR message");
+
+        logger.debug("LOGBACK KATA - Test DEBUG message");
+        logger.trace("LOGBACK KATA - Test TRACE message");
     }
 
 }
